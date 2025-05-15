6278901c78ced5d1d49bd826b8881f6228ad1d97
MSAFVANKV <mskvphed@gmail.com>
2025-05-15 21:51:34 +0530

    last update for background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <main className="">
+        <main className="bg-white">
           <Header />
         {children}  
         </main>
