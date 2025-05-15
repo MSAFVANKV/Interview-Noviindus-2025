1d49c7dc8aba5301f3340ee660cd9505083ed901
MSAFVANKV <mskvphed@gmail.com>
2025-05-15 20:56:43 +0530

    completed footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,32 +1,110 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
-type Props = {}
-
-const Footer = (props: Props) => {
+const Footer = () => {
   return (
-    <footer className=' bg-main sm:py-20  py-10 md:px-20 sm:px-8 px-3 space-y-7 grid grid-cols-6'>
-
-        {/* 1. logo */}
-        <div className="flex items-center gap-2 col-span-2 bg-amber-50">
-          <div className="w-10 h-10 relative">
-            <Image fill alt="logo" src={`/logo/logo.svg`} />
-          </div>
-          <h1 className="text-xl font-bold text-white">Educare</h1>
+    <footer className=" bg-main   space-y-7 ">
+     <div className="grid md:grid-cols-6 sm:grid-cols-3 md:px-20 sm:px-8 px-3  py-10">
+       {/* 1. logo */}
+       <div className="flex items-center gap-2 md:col-span-2 ">
+        <div className="w-10 h-10 relative">
+          <Image
+            src="/logo/logo.svg"
+            alt="logo"
+            fill
+            className="object-contain"
+          />
         </div>
+        <h1 className="text-xl font-bold text-white">Educare</h1>
+      </div>
 
-        {/* 2 */}
-        <div className="bg-amber-100 w-full"></div>
+      {/* 2 */}
+      <ul className=" w-full flex flex-col gap-1  text-white">
+        {[
+          "Navigation",
+          "Home",
+          "About",
+          "Curriculum",
+          "Career",
+          "Blogs",
+          "Contact us",
+        ].map((item, index) => (
+          <li
+            className={`${
+              index === 0 ? "font-bold mb-4" : ""
+            } cursor-pointer hover:underline underline-offset-4`}
+            key={index}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
 
-          {/* 3 */}
-          <div className="bg-amber-200 w-full"></div>
+      {/* 3 */}
+      <ul className=" w-full flex flex-col gap-1  text-white">
+        {[
+          "Contact info",
+          "educare@gmail.com",
+          "info@educare.com",
+          "+974 4412 3456",
+          "+974 5534 7890",
+        ].map((item, index) => (
+          <li
+            className={`${
+              index === 0 ? "font-bold mb-4" : ""
+            } cursor-pointer hover:underline underline-offset-4`}
+            key={index}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
 
-            {/* 4 */}
-        <div className="bg-amber-300 w-full col-span-2"></div>
-        
-            
+      {/* 4 */}
+      <div className="h-full w-full relative">
+        <Image
+          src="/properities/footer-map.svg"
+          alt="article image"
+          fill
+          className="object-contain w-40"
+        />
+      </div>
+
+      {/* 5 */}
+      <ul className=" w-full flex flex-col gap-1  text-white">
+        {[
+          "Office Address ",
+          "EducareEducation CenterBuilding 45, Al Sadd Street,Doha, Qatar",
+        ].map((item, index) => (
+          <li
+            className={`${
+              index === 0 ? "font-bold mb-4" : ""
+            } cursor-pointer hover:underline underline-offset-4`}
+            key={index}
+          >
+            {item}
+          </li>
+        ))}
+        <span className="text-xs underline mt-3">
+        View on Map
+        </span>
+      </ul>
+     </div>
+
+      <div className="border-t flex justify-between sm:flex-row flex-col items-center p-5 text-xs w-full">
+        <span className="text-white">
+        © 2024 educare
+        </span>
+        <span className="flex items-center gap-1 text-white">
+        Designed By <p className="text-[#DCC196]">Noviindus Technologies</p>
+        </span>
+        <span className="text-[#DCC196]">
+        Terms & Conditions Privacy Policy
+        </span>
+
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
